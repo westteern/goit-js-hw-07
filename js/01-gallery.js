4fe70baf8a59f05507fc01522ff115ccbea1ae25
Pavel Mikhaylenko <mikhaylenko.p@gmail.com>
2022-09-21 12:32:03 +0300

    add_markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = galleryItems;
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+const markup = gallery
+	.map(
+		image => `<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="large-image.jpg"
+      alt="${image.description}"
+    />
+  </a>
+</div>`,
+	)
+	.join(" ");
+
+list.innerHTML = markup;
