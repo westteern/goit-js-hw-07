f141717b96d176cdb594062f2d8c41bbbef2fa22
Pavel Mikhaylenko <104693653+westteern@users.noreply.github.com>
2022-09-22 00:49:19 +0300

    add_task_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const gallery = galleryItems;
 
-const list = document.querySelector(".gallery");
+const listRef = document.querySelector(".gallery");
 
 const markup = gallery
 	.map(
@@ -11,7 +11,7 @@ const markup = gallery
     <img
       class="gallery__image"
       src="${image.preview}"
-      data-source="large-image.jpg"
+      data-source="${image.original}"
       alt="${image.description}"
     />
   </a>
@@ -19,4 +19,28 @@ const markup = gallery
 	)
 	.join(" ");
 
-list.innerHTML = markup;
+listRef.innerHTML = markup;
+
+listRef.addEventListener("click", showImageFullSizeInModal);
+
+function showImageFullSizeInModal(e) {
+	e.preventDefault();
+	if (!e.target.classList.contains("gallery__image")) {
+		return;
+	}
+	const instance = basicLightbox.create(`
+	    <div class="modal">
+	        <img src="${e.target.dataset.source}">
+	    </div>
+	`);
+	instance.show();
+
+	document.addEventListener("keydown", closeModal);
+
+	function closeModal(e) {
+		if (e.code === "Escape") {
+			instance.close();
+			document.removeEventListener("keydown", closeModal);
+		}
+	}
+}
